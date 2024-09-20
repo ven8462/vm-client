56b42daabb3e1e41aeb3410d026eec9f9aafed12
ven8462 <lavenderanyango9@gmail.com>
2024-09-20 17:24:49 +0300

    feat: main app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './LandingPage';
+import LoginForm from './LoginForm';
+import Signup from './Signup';
+import UserDashboard from './UserDashboard';
+import CreateBackup from './CreateBackup';
+import Billing from './Billing';
+import SubscriptionManagement from './SubscriptionManagement';
+import AdminDashboard from './AdminDashBoard';
+import CreateVirtualMachine from './CreateVirtualMachines';
+import ManageVirtualMachine from './ManageVirtualMachines';
+import VirtualMachineList from './RemoveVirtualMachine';
+import AssignVirtualMachine from './AssignVirtualMachine';
+import GuestDashboard from './GuestDashboard';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/guest" element={<GuestDashboard />} />
+
+          <Route path='admin' element={<AdminDashboard />}>
+            <Route path="create-vm" element={<CreateVirtualMachine />} />
+            <Route path="manage-vm" element={<ManageVirtualMachine />} />
+            <Route path="delete-vm" element={<VirtualMachineList  />} />
+            <Route path="assign-vm" element={<AssignVirtualMachine  />} />
+          </Route>
+
+          {/* Nested routes inside UserDashboard */}
+          <Route path="/account" element={<UserDashboard />}>
+            <Route path="vm-management" element={<GuestDashboard />} />
+            <Route path="create-backup" element={<CreateBackup />} />
+            <Route path="billing" element={<Billing />} />
+            <Route path="subscription-management" element={<SubscriptionManagement />} />
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
